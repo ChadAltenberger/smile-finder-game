cd0fdbbd6f52ab66dfe794f382982979f7c1e9a5
Chad Altenberger <chadaltenberger@gmail.com>
2020-11-02 16:49:30 -0500

    Added difficulty selector and game reset functionality and some styling tweaks

--- main.js
+++ main.js
@@ -1,11 +1,54 @@
-let numberOfFaces = 5;
 const theLeftSide = document.querySelector('#leftSide');
 const theRightSide = document.querySelector('#rightSide');
 
-let numTries = 0;
+const easyNumFaces = 2;
+const normalNumFaces = 3;
+const hardNumFaces = 5;
 
-function generateFaces() {
-  for (i = 0; i < numberOfFaces; i++) {
+let numTries = 0;
+let numberOfFaces;
+
+// Difficulty Level buttons
+const easyBtn = document.querySelector('#easy');
+const normalBtn = document.querySelector('#normal');
+const hardBtn = document.querySelector('#hard');
+
+// Button click events
+easyBtn.addEventListener('click', easyLevelHandler);
+normalBtn.addEventListener('click', normalLevelHandler);
+hardBtn.addEventListener('click', hardLevelHandler);
+
+// Button click handler functions
+function easyLevelHandler() {
+  generateFaces(easyNumFaces);
+  theLeftSide.scrollIntoView();
+  easyBtn.removeEventListener('click', easyLevelHandler);
+  normalBtn.removeEventListener('click', normalLevelHandler);
+  hardBtn.removeEventListener('click', hardLevelHandler);
+  numberOfFaces = 2;
+}
+
+function normalLevelHandler() {
+  generateFaces(normalNumFaces);
+  theLeftSide.scrollIntoView();
+  easyBtn.removeEventListener('click', easyLevelHandler);
+  normalBtn.removeEventListener('click', normalLevelHandler);
+  hardBtn.removeEventListener('click', hardLevelHandler);
+  numberOfFaces = 3;
+}
+
+function hardLevelHandler() {
+  generateFaces(hardNumFaces);
+  theLeftSide.scrollIntoView();
+  easyBtn.removeEventListener('click', easyLevelHandler);
+  normalBtn.removeEventListener('click', normalLevelHandler);
+  hardBtn.removeEventListener('click', hardLevelHandler);
+  numberOfFaces = 5;
+}
+
+// Gernerate Faces function
+function generateFaces(number) {
+  for (i = 0; i < number; i++) {
     let face = document.createElement('img');
     face.src = '../assets/images/smile.png';
 
@@ -19,20 +62,14 @@ function generateFaces() {
 
   const leftSideImages = theLeftSide.cloneNode(true);
   leftSideImages.removeChild(leftSideImages.lastChild);
+  theLeftSide.addEventListener('click', gameOver);
   theRightSide.appendChild(leftSideImages);
 
-  theLeftSide.lastChild.addEventListener('click', nextLevel);
-  document.body.addEventListener('click', gameOver);
+  theLeftSide.lastChild.addEventListener('click', nextRound);
 }
 
-function gameOver() {
-  alert(`Game over! You completed ${numTries} attempts!`);
-
-  document.body.removeEventListener('click', gameOver);
-  leftSideImages.removeEventListener('click', nextLevel);
-}
-
-function nextLevel() {
+// Next Round function
+function nextRound() {
   event.stopPropagation();
 
   while (theLeftSide.firstChild) {
@@ -42,8 +79,39 @@ function nextLevel() {
     theRightSide.removeChild(theRightSide.firstChild);
   }
 
-  numberOfFaces += 5;
-  generateFaces();
+  numberOfFaces += numberOfFaces;
+  generateFaces(numberOfFaces);
 
   numTries++;
 }
+
+// Game Over function
+function gameOver() {
+  alert(`Game over! You completed ${numTries} rounds!`);
+
+  function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }
+  }
+  const lContainer = document.querySelector('#leftSide');
+  const rContainer = document.querySelector('#rightSide');
+  removeAllChildNodes(lContainer);
+  removeAllChildNodes(rContainer);
+
+  alert('Play again');
+
+  window.scrollTo(0, 0);
+  // gameIsRunning = false;
+
+  easyBtn.addEventListener('click', easyLevelHandler);
+  normalBtn.addEventListener('click', normalLevelHandler);
+  hardBtn.addEventListener('click', hardLevelHandler);
+
+  // document.body.removeEventListener('click', gameOver);
+  leftSideImages.removeEventListener('click', nextRound);
+}
+
+window.onbeforeunload = () => {
+  window.scrollTo(0, 0);
+};
