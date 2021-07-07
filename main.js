fab11eef09f3c0945c7bf908308883ffc7ccee83
Chad Altenberger <chadaltenberger@gmail.com>
2021-07-07 11:24:14 -0400

    Redesigned the UI and added responsive styling/features

--- main.js
+++ main.js
@@ -19,99 +19,110 @@ normalBtn.addEventListener('click', normalLevelHandler);
 hardBtn.addEventListener('click', hardLevelHandler);
 
 const turnOffButtons = () => {
-  easyBtn.removeEventListener('click', easyLevelHandler);
-  normalBtn.removeEventListener('click', normalLevelHandler);
-  hardBtn.removeEventListener('click', hardLevelHandler);
+	easyBtn.removeEventListener('click', easyLevelHandler);
+	normalBtn.removeEventListener('click', normalLevelHandler);
+	hardBtn.removeEventListener('click', hardLevelHandler);
 };
 
 // Button click handler functions
 function easyLevelHandler() {
-  generateFaces(easyNumFaces);
-  theLeftSide.scrollIntoView();
-  turnOffButtons();
-  numberOfFaces = 2;
+	generateFaces(easyNumFaces);
+	theLeftSide.scrollIntoView();
+	turnOffButtons();
+	numberOfFaces = 2;
 }
 
 function normalLevelHandler() {
-  generateFaces(normalNumFaces);
-  theLeftSide.scrollIntoView();
-  turnOffButtons();
-  numberOfFaces = 3;
+	generateFaces(normalNumFaces);
+	theLeftSide.scrollIntoView();
+	turnOffButtons();
+	numberOfFaces = 3;
 }
 
 function hardLevelHandler() {
-  generateFaces(hardNumFaces);
-  theLeftSide.scrollIntoView();
-  turnOffButtons();
-  numberOfFaces = 5;
+	generateFaces(hardNumFaces);
+	theLeftSide.scrollIntoView();
+	turnOffButtons();
+	numberOfFaces = 5;
 }
 
 // Gernerate Faces function
 function generateFaces(number) {
-  for (i = 0; i < number; i++) {
-    let face = document.createElement('img');
-    face.src = 'https://chadaltenberger.github.io/smile-finder-game/smile.png';
+	for (i = 0; i < number; i++) {
+		let face = document.createElement('img');
+		face.src = 'https://chadaltenberger.github.io/smile-finder-game/smile.png';
 
-    let randomTop = Math.floor(Math.random() * 400) + 1;
-    let randomLeft = Math.floor(Math.random() * 400) + 1;
-    face.style.top = randomTop + 'px';
-    face.style.left = randomLeft + 'px';
+		// Check screen size: if small then make the random area smaller
+		if (window.innerWidth < 500) {
+			let randomTop = Math.floor(Math.random() * 100) + 1;
+			let randomLeft = Math.floor(Math.random() * 100) + 1;
+			face.style.top = randomTop + 'px';
+			face.style.left = randomLeft + 'px';
+		} else {
+			let randomTop = Math.floor(Math.random() * 400) + 1;
+			let randomLeft = Math.floor(Math.random() * 400) + 1;
+			face.style.top = randomTop + 'px';
+			face.style.left = randomLeft + 'px';
+		}
 
-    theLeftSide.appendChild(face);
-  }
+		theLeftSide.appendChild(face);
+	}
 
-  const leftSideImages = theLeftSide.cloneNode(true);
-  leftSideImages.removeChild(leftSideImages.lastChild);
-  theLeftSide.addEventListener('click', gameOver);
-  theRightSide.appendChild(leftSideImages);
+	const leftSideImages = theLeftSide.cloneNode(true);
+	leftSideImages.removeChild(leftSideImages.lastChild);
+	theLeftSide.addEventListener('click', gameOver);
+	theRightSide.appendChild(leftSideImages);
 
-  theLeftSide.lastChild.addEventListener('click', nextRound);
+	theLeftSide.lastChild.addEventListener('click', nextRound);
 }
 
 // Next Round function
 function nextRound() {
-  event.stopPropagation();
+	event.stopPropagation();
 
-  while (theLeftSide.firstChild) {
-    theLeftSide.removeChild(theLeftSide.firstChild);
-  }
-  while (theRightSide.firstChild) {
-    theRightSide.removeChild(theRightSide.firstChild);
-  }
+	while (theLeftSide.firstChild) {
+		theLeftSide.removeChild(theLeftSide.firstChild);
+	}
+	while (theRightSide.firstChild) {
+		theRightSide.removeChild(theRightSide.firstChild);
+	}
 
-  numberOfFaces += numberOfFaces;
-  generateFaces(numberOfFaces);
+	numberOfFaces += numberOfFaces;
+	generateFaces(numberOfFaces);
 
-  numTries++;
+	numTries++;
 }
 
 // Game Over function
 function gameOver() {
-  alert(`Game over! You completed ${numTries} rounds!`);
+	alert(`Game over! You completed ${numTries} rounds!`);
 
-  function removeAllChildNodes(parent) {
-    while (parent.firstChild) {
-      parent.removeChild(parent.firstChild);
-    }
-  }
-  const lContainer = document.querySelector('#leftSide');
-  const rContainer = document.querySelector('#rightSide');
-  removeAllChildNodes(lContainer);
-  removeAllChildNodes(rContainer);
+	function removeAllChildNodes(parent) {
+		while (parent.firstChild) {
+			parent.removeChild(parent.firstChild);
+		}
+	}
+	const lContainer = document.querySelector('#leftSide');
+	const rContainer = document.querySelector('#rightSide');
+	removeAllChildNodes(lContainer);
+	removeAllChildNodes(rContainer);
 
-  alert('Play again');
+	alert('Play again');
 
-  window.scrollTo(0, 0);
-  // gameIsRunning = false;
+	window.scrollTo(0, 0);
+	// gameIsRunning = false;
 
-  easyBtn.addEventListener('click', easyLevelHandler);
-  normalBtn.addEventListener('click', normalLevelHandler);
-  hardBtn.addEventListener('click', hardLevelHandler);
+	easyBtn.addEventListener('click', easyLevelHandler);
+	normalBtn.addEventListener('click', normalLevelHandler);
+	hardBtn.addEventListener('click', hardLevelHandler);
 
-  // document.body.removeEventListener('click', gameOver);
-  leftSideImages.removeEventListener('click', nextRound);
+	// document.body.removeEventListener('click', gameOver);
+	leftSideImages.removeEventListener('click', nextRound);
 }
 
-window.onbeforeunload = () => {
-  window.scrollTo(0, 0);
-};
+window.onbeforeunload = () => window.scrollTo(0, 0);
+
+if (window.innerWidth < 500) {
+	let text = document.querySelector('.instructions-text');
+	text.innerHTML = 'Click on the extra smiley face on the top.';
+}
